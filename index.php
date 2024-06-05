7a294daa25bedc83ded66d9f48c643aca37837d4
darmanta <darman1407@gmail.com>
2024-06-05 20:45:25 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h1>Latihan Git Remote</h1>
+    <br>
+    <h1>Darmanta Sukrianto</h1>
+</body>
+
+</html>
